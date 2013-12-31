51e824c7eef3888f6ba39101d1c02f2d1949c0a8
Joshua Ball <joshball@yahoo-inc.com>
2013-12-31 11:51:52 -0800

    close enough

--- src/com/sciolizer/intellihack/HackConfiguraitonType.java
+++ src/com/sciolizer/intellihack/HackConfiguraitonType.java
@@ -8,19 +8,25 @@ import com.intellij.execution.configurations.*;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.ProgramRunner;
+import com.intellij.execution.ui.ConfigurationModuleSelector;
 import com.intellij.execution.ui.ExecutionConsole;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.WriteExternalException;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.search.GlobalSearchScope;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.io.OutputStream;
 import java.util.Collection;
 
@@ -41,25 +47,56 @@ public class HackConfiguraitonType extends ConfigurationTypeBase {
 
         private String runnableClass = "";
         private final SettingsEditor<HackRunProfile> hackSettingsEditor = new SettingsEditor<HackRunProfile>() {
-            private final JPanel jPanel = new JPanel();
-            private final JTextArea jTextArea = new JTextArea();
+            private final JPanel jPanel;
+            private final JTextArea runnableClassTextArea;
+            private final ConfigurationModuleSelector configurationModuleSelector;
 
             {
-                JLabel jLabel = new JLabel("Runnable class:");
-                jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.X_AXIS));
-                jPanel.add(jLabel);
-                jPanel.add(jTextArea);
+                JLabel runnableClassLabel = new JLabel("Runnable class:");
+
+                runnableClassTextArea = new JTextArea(runnableClass);
+
+                JPanel runnableClassPanel = new JPanel();
+                runnableClassPanel.setLayout(new BoxLayout(runnableClassPanel, BoxLayout.X_AXIS));
+                runnableClassPanel.add(runnableClassLabel);
+                runnableClassPanel.add(runnableClassTextArea);
+
+                JLabel moduleLabel = new JLabel("Module:");
+
+                ComboBox moduleComboBox = new ComboBox();
+                configurationModuleSelector = new ConfigurationModuleSelector(getProject(), moduleComboBox);
+
+                JPanel modulePanel = new JPanel();
+                modulePanel.setLayout(new BoxLayout(modulePanel, BoxLayout.X_AXIS));
+                modulePanel.add(moduleLabel);
+                modulePanel.add(moduleComboBox);
+
+                jPanel = new JPanel();
+                jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+                jPanel.add(runnableClassPanel);
+                jPanel.add(modulePanel);
+
+                moduleComboBox.addActionListener(new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("actionPerformed: " + configurationModuleSelector.getModule());
+                        setModule(configurationModuleSelector.getModule());
+                    }
+                });
+                setModule(configurationModuleSelector.getModule());
             }
 
             @Override
             protected void resetEditorFrom(HackRunProfile s) {
-                jTextArea.setText(s.runnableClass);
+                runnableClassTextArea.setText(s.runnableClass);
+                configurationModuleSelector.reset(HackRunProfile.this);
             }
 
             @Override
             protected void applyEditorTo(HackRunProfile s) throws ConfigurationException {
-                String text = jTextArea.getText();
+                String text = runnableClassTextArea.getText();
                 s.runnableClass = text;
+                configurationModuleSelector.applyTo(HackRunProfile.this);
             }
 
             @NotNull
@@ -76,6 +113,7 @@ public class HackConfiguraitonType extends ConfigurationTypeBase {
         @Override
         public void readExternal(Element element) throws InvalidDataException {
             runnableClass = element.getChild("runnableClass").getAttribute("class").getValue();
+            getConfigurationModule().readExternal(element);
         }
 
         @Override
@@ -83,6 +121,23 @@ public class HackConfiguraitonType extends ConfigurationTypeBase {
             Element child = new Element("runnableClass");
             child.setAttribute("class", runnableClass);
             element.addContent(child);
+            getConfigurationModule().writeExternal(element);
+        }
+
+        @Override
+        public void checkConfiguration() throws RuntimeConfigurationException {
+            if (runnableClass == null || runnableClass.isEmpty()) {
+                throw new RuntimeConfigurationError("Class not specified.");
+            }
+            Module module = getConfigurationModule().getModule();
+            if (module == null) {
+                throw new RuntimeConfigurationError("Module not specified.");
+            }
+            PsiClass aClass = JavaPsiFacade.getInstance(getProject()).findClass(runnableClass, GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module, true));
+            if (aClass == null) {
+                throw new RuntimeConfigurationError("Could not find class '" + runnableClass + "'");
+            }
+            // todo: if module unspecified but class can be found in project (and is unambiguous), create quick fix for setting the module.
         }
 
         @Override
@@ -187,6 +242,8 @@ public class HackConfiguraitonType extends ConfigurationTypeBase {
         private HackRunConfigurationModule(Project project) {
             super(project);
         }
+
+
     }
 
 }
